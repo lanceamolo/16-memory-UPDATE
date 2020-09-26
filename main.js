8c7991c9051b2de50d76ba783b1bff7402606184
Punchcode <punchcode@Punchcodes-MacBook-Pro.local>
2020-09-25 17:38:38 -0700

    Update

--- main.js
+++ main.js
@@ -9,8 +9,74 @@
 // Focus on getting the actual game to work first! Then focus on the next feature.
 // Understand how cards turn
 
+// shuffle deck of cards and place them on the board
+// const chosenWord = allowedWords[Math.floor(Math.random() * allowedWords.length)]
+// console.log(chosenWord)
+// const gbpItemTitle = gbpItem.map(function (item) {
+//   return `${item.title} $${item.price}`
+// })
+// document.querySelector("#answer3").innerHTML = `${gbpItemTitle}`
+
+const deck = [
+  "variables",
+  "variables",
+  "operators",
+  "operators",
+  "data type",
+  "data type",
+  "primitives",
+  "primitives",
+  "objects",
+  "objects",
+  "data access.[]",
+  "data access.[]",
+  "conditional statements",
+  "conditional statements",
+  "methods",
+  "methods",
+  "functions",
+  "functions",
+]
+
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
+  }
+
+  return array
+}
+
+// start screen trigger the 1st shuffle
+window.onload = function () {
+  document.getElementById("buttonModal").onclick = function () {
+    document.getElementById("modal").style.display = "none"
+  }
+}
+
+const deckShuffle = shuffle(deck)
+console.log(deck)
+
+let gameGrid = []
+for (let j = 0; j < deckShuffle.length; j++) {
+  gameGrid = deckShuffle[j]
+  console.log(gameGrid)
+  document.getElementsByClassName("slot")[j].innerHTML = `${gameGrid}`
+}
+
 // When the user finds a match, leave the cards face up and disallow clicking those cards
-const slots = document.querySelectorAll(".slots")
+const slot = document.querySelectorAll(".slot")
 
 // empty variables for clicked on data
 let card1
@@ -41,7 +107,7 @@ function flipCard() {
 }
 
 function matching() {
-  if (card1.dataset.title !== card2.dataset.title) {
+  if (card1.innerHTML !== card2.innerHTML) {
     // hide cards again if they dont match data attribute
     unflipCards()
     setTimeout(() => {
@@ -58,7 +124,7 @@ function unflipCards() {
 }
 
 // need to be able to click on each card to show bottom side
-slots.forEach((card) => card.addEventListener("click", flipCard))
+slot.forEach((card) => card.addEventListener("click", flipCard))
 
 // const slots = document.querySelectorAll(".card")
 
